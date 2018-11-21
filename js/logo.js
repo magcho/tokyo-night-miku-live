ad458ed9fd4f1a48f19d77cf74858ed7d57b56db
magcho <mail@magcho.com>
2018-11-22 01:55:29 +0900

    event書いた

--- js/logo.js
+++ js/logo.js
@@ -1,11 +1,29 @@
-new Vivus('svg-anime', {
-  type: 'delayed',
-  duration: 100,
-  animTimingFunction: Vivus.EASE ,
-  file: './img/logo-animate.svg'
-}, callback);
 
-function callback(){
+const debug  = true
+
+const callback = () =>{
   document.getElementById('logo2').style.opacity = 1
   document.getElementById('logo1').style.opacity = 0
+  headerHegiht();
+  // document.getElementById('blind-cover').style.opacity = 0
+}
+const headerHegiht = () =>{
+  document.getElementsByTagName('header')[0].style.height = document.getElementById('logo1').clientHeight;
+}
+
+
+if(debug){
+  new Vivus('svg-anime', {
+    type: 'delayed',
+    duration: 1,
+    animTimingFunction: Vivus.EASE ,
+    file: './img/logo-animate.svg'
+  }, callback);
+}else{
+  new Vivus('svg-anime', {
+    type: 'delayed',
+    duration: 100,
+    animTimingFunction: Vivus.EASE ,
+    file: './img/logo-animate.svg'
+  }, callback);
 }
